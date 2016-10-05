b95992dbec29ccb7ccf63126399660d135200cbd
Moritz Schönherr <moritz.schoenherr@gmail.com>
2016-10-05 08:04:56 +0200

    rundamentary day/month display for gamedate

--- date.py
+++ date.py
@@ -3,10 +3,23 @@ class GameDate():
     def __init__(self):
 
         self.intdate = 0 
+        self.date_ranges = [[0,30],[31,59],[60,90],[91,110],[111,141],[142,171],[172,202],[203,233],[234,263],[264,294],[295,324],[325,365]]
+        #that is not correct, yet
 
-    def getStringRep():
+    def getStringRep(self):
 
-        return str(self.intdate)
+        month = 0 
+        day = 0
+
+        for ind, rang in enumerate(self.date_ranges):
+
+            if self.intdate in rang:
+
+                month = ind + 1
+                day = self.intdate - min(rang) + 1
+                break
+
+        return str(day) + "/" + str(month)
 
     def update(self):
 
