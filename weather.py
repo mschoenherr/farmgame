ca0bdf2c4971c43cd394645f486064df07b0e103
Moritz Schönherr <moritz.schoenherr@gmail.com>
2016-10-08 13:20:27 +0200

    more stable trend, mean maximum temp displayed

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ class Weather():
 
     def __init__(self):
 
-        self.target_temperature = [0,3,5,9,15,19,20,19,18,11,8,7]
+        self.target_temperature = [2,4,8,12,19,22,24,24,19,14,7,4]
         self.target_rain = [50,30,38,50,25,60,60,80,45,70,90,30]
         self.target_sun = [15,40,50,80,90,80,90,80,50,40,30,40]
 
@@ -35,8 +35,9 @@ class Weather():
 
         det_trend = self.target_sun[month] - self.sun 
         rand_trend = randint(0,100) - self.sun
+        prev_tendency = self.sun_tendency
         
-        self.sun_tendency = trunc(0.7 * det_trend + 0.3 * rand_trend)
+        self.sun_tendency = self.trendHelper(det_trend,rand_trend,prev_tendency)
 
     def setRainTendency(self,date):
 
@@ -44,8 +45,9 @@ class Weather():
 
         det_trend = self.target_rain[month] - self.rain
         rand_trend = randint(0,100) - self.rain
+        prev_tendency = self.rain_tendency
         
-        self.rain_tendency = trunc(0.7 * det_trend + 0.3 * rand_trend)
+        self.rain_tendency = self.trendHelper(det_trend,rand_trend,prev_tendency)
 
     def setTemperatureTendency(self,date):
 
@@ -53,9 +55,15 @@ class Weather():
 
         det_trend = self.target_temperature[month] - self.temperature
         rand_trend = randint(-10,40) - self.temperature
+        prev_tendency = self.temperature_tendency
         
-        self.temperature_tendency = trunc(0.7 * det_trend + 0.3 * rand_trend)
+        self.temperature_tendency = self.trendHelper(det_trend,rand_trend,prev_tendency)
 
+    def trendHelper(self,det,ran,prev):
+
+        trend = 0.7 * det + 0.15 * ran + 0.15 * prev
+
+        return trunc(trend)
 
         # in the future, calculate everything depending on date and prev weather
 
